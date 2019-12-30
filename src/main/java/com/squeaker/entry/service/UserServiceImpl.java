33c762ee86b48da14ae126fbf9b777d3845d63fe
LeagueLugas <blackblack9685@gmail.com>
2019-12-30 20:20:06 +0900

    Uri prefix는 못하겠어요

--- src/main/java/com/squeaker/entry/service/UserServiceImpl.java
+++ src/main/java/com/squeaker/entry/service/UserServiceImpl.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
